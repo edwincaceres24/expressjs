0c324a290d066b06eddb8764c98cbf887ee5bde3
Edwin Caceres <administrador@MacBook-Pro-de-Administrador.local>
2022-01-16 01:48:39 -0500

    Get

--- app.js
+++ app.js
@@ -2,23 +2,34 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-app.get('/',(req,res)=>{
+app.get('/',(req,res)=>{ //We always use req and response as paramenters
 		res.send('Basic Express Setup')
 })
 
-app.post('/',(req,res)=>{
-		res.send('Got a Post Request')
+app.get('/profiles',(req,res)=>{
+		res.json([{
+				name : "Edwin",
+				profile: "Javascript Developer"
+		},
+		{
+				name: "Nathalie",
+				profile: "Data"
+		}])
 })
 
-app.put('/user',(req,res)=>{
-		res.send('Got a Put request at /user')
+app.get('/users/:profileId/products/:productId',(req,res)=>{
+	const { profileId, productId  } = req.params;
+		res.json({
+				profileId,
+				productId,
+				name: 'Emilia',
+				profile: 'Lego Developer'
+		})
 })
 
 app.delete('/user',(req,res)=>{
-		res.send('Got a DELETE request at /user')
-})
+		res.send('Got a DELETE request at /user')})
 
 app.listen(port, ()=>{
-		console.log(`Listening port ${port}`)
+		console.log(`Listening port ${port}`) //We use a callback just to confirm that we're listening the port
 })
-

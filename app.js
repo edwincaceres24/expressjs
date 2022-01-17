26a086f70797c01d98a6b03ae78ae3c5366d4019
Edwin Caceres <administrador@MacBook-Pro-de-Administrador.local>
2022-01-17 00:15:25 -0500

    Learning router

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const faker = require("faker")
+
 
 app.get('/',(req,res)=>{ //We always use req and response as paramenters
 		res.send('Basic Express Setup')
 })
 
-app.get('/profiles',(req,res)=>{
+app.get('/products/filter',(req,res)=>{ //Put specific routes before dynamuc routes
+	res.send("I'm a filter")	
+})
+
+app.get('/products/:id',(req,res)=>{
+		const {id} = req.params;
+
 		res.json([{
+				id,
 				name : "Edwin",
 				profile: "Javascript Developer"
 		},
@@ -15,6 +24,20 @@ app.get('/profiles',(req,res)=>{
 				name: "Nathalie",
 				profile: "Data"
 		}])
+})
+
+app.get('/products',(req,res)=>{
+	const products = []
+	const {size} = req.query
+
+	for (let index = 0; index < size; index++){
+			products.push({
+				name: faker.commerce.productName(),
+				price: parseInt(faker.commerce.price(),10),
+				image: faker.image.imageUrl()
+			})
+	}
+	res.json(products)
 })
 
 app.get('/users/:profileId/products/:productId',(req,res)=>{
